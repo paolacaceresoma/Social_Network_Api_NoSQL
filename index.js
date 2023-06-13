622f5fb82d427249df1050e4eedb53fd21b6f726
Paola Caceres <paolacaceresoma@gmail.com>
2023-06-13 02:07:41 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const userRoutes = require("./userController");
+const thoughtRoutes = require("./thoughtController");
+
+router.use("/users", userRoutes);
+router.use("/thoughts", thoughtRoutes);
+module.exports = router;
